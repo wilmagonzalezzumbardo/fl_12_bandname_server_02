86b71eebc7c01cbf4fa5b825189aecfffe22298d
Wilma <miguel_moreno20@hotmail.com>
2022-11-25 21:49:18 -0600

    251122 0949

--- sockets/socket.js
+++ sockets/socket.js
@@ -16,7 +16,7 @@ io.on('connection', client =>
     console.log("Mensaje1, cliente conectado");
     console.log(today);
     
-    console.log(client.handshake.headers):
+    console.log(client.handshake.headers);
 
     console.log ("on connection INI");
     var sLista = bands.getBands();
